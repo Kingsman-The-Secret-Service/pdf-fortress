3f35060873f241c9424c12081f733d932576083f
Kaviarasan K K <kaviarasankk@gmail.com>
2017-06-03 20:09:18 +0530

    Purge unwanted lines

--- fortress-gui.py
+++ fortress-gui.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'demo.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
-#
-# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
